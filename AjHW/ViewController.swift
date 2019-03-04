cd0a9b6019872eccc899234109b04da835a15e7a
ElenaLiu <elena.liu@hwacom.com>
2019-03-04 22:24:33 +0800

    Initial Commit

---
+++ AjHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AjHW
+//
+//  Created by 劉芳瑜 on 2019/3/4.
+//  Copyright © 2019 Fang-Yu. Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
